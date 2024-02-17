54ad29a4bd83b2d659a713801a8640174ef8a211
Natachakomko <97403733+Natachakomko@users.noreply.github.com>
2024-02-17 15:35:27 -0500

    home page

---
+++ src/app/menubar-main/menubar-main.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menubar-main',
+  templateUrl: './menubar-main.component.html',
+  styleUrl: './menubar-main.component.css'
+})
+export class MenubarMainComponent {
+
+}
