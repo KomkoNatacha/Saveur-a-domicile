50009011748a2a539f367ac3a12dff7c54054918
Natachakomko <97403733+Natachakomko@users.noreply.github.com>
2024-02-21 13:51:16 -0500

    homev

--- src/app/menubar-main/menubar-main.component.ts
+++ src/app/menubar-main/menubar-main.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-menubar-main',
   templateUrl: './menubar-main.component.html',
   styleUrl: './menubar-main.component.css'
 })
-export class MenubarMainComponent {
+export class MenubarMainComponent implements  OnInit{
 
+  isHomeActive: boolean = false;
+
+  constructor(private  route : ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.url.subscribe(urlSegments =>{
+      this.isHomeActive = urlSegments[0].path=='';
+    } )
+  }
 }
