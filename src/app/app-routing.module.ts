fbada9f65510678ee1f9a6d86b70cd00b1d02074
William Demanou <William.dema@yahoo.com>
2024-03-06 22:42:57 -0500

    suite dernier commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,6 @@ const routes: Routes = [
   {path: 'home-restaurant', component : RestaurantHomeComponent},
   { path: '', redirectTo: 'home', pathMatch: "full" },
   { path: '**', redirectTo: 'home', pathMatch: "full" },
-
 ];
 
 @NgModule({
